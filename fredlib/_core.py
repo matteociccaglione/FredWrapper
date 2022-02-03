fd411d7f15ee9b833c097b3a01bd4c0ec3919443
Federica <federica_vil@yahoo.it>
2022-02-03 12:44:22 +0100

    Categories added in db

--- fredlib/_core.py
+++ fredlib/_core.py
@@ -7,14 +7,17 @@ import sqlite3
 from typing import List, Any
 from datetime import datetime as dt
 
-_database_configuration = {"tables": ["series", "observables"],
+_database_configuration = {"tables": ["series", "observables", "categories"],
                            "rows": {
                                "series": [("series_id", "TEXT PRIMARY KEY"), ("title", "TEXT"),
                                           ("last_updated", "TEXT"),
-                                          ("category_id", "TEXT")],
+                                          ("category_id",
+                                           "TEXT REFERENCES categories(category_id) ON DELETE CASCADE ON UPDATE CASCADE")],
                                "observables": [("id", "INTEGER PRIMARY KEY AUTOINCREMENT"), ("date", "TEXT"),
                                                ("value", "REAL"), ("series_id",
-                                                                   "TEXT REFERENCES series(series_id) ON DELETE CASCADE ON UPDATE CASCADE")]
+                                                                   "TEXT REFERENCES series(series_id) ON DELETE CASCADE ON UPDATE CASCADE")],
+                               "categories": [("category_id", "TEXT PRIMARY KEY"), ("name", "TEXT"),
+                                              ("parent_id", "TEXT")]
                            }}
 
 
@@ -113,7 +116,6 @@ class Fred(DataManager):
 
     def _parse(self, json_object, model_type, elem_id=0) -> List[Any]:
         dictionary = json.loads(json_object)
-        result = []
         if model_type == ModelType.Category:
             categories = dictionary["categories"]
             returned_categories = []
@@ -224,10 +226,28 @@ class Database(DataManager):
                 observables.append(observable)
             return observables
 
+        if model_type == ModelType.Category:
+            categories = []
+            for row in rows:
+                category = Category(row[0],row[1],row[2])
+                categories.append(category)
+            return categories
+
         raise NotSupportedModelType(model_type)
 
     def get_category(self, category_id) -> Category:
-        raise NotSupportedOperation()
+        statement = "SELECT * FROM categories WHERE category_id =" + str(category_id) + ";"
+        rows = self._get(statement)
+        categories = self._parse(ModelType.Category, rows)
+        if len(categories) > 0:
+            return categories[0]
+        else:
+            raise CategoryNotFound(category_id)
+
+    def get_categories_by_parent_id(self,parent_id) -> []:
+        statement = "SELECT * FROM categories WHERE parent_id=" + str(parent_id) + ";"
+        rows = self._get(statement)
+        return self._parse(ModelType.Category, rows)
 
     def get_series(self, category) -> []:
         statement = "SELECT * FROM series WHERE category_id = " + str(category) + ";"
@@ -249,8 +269,13 @@ class Database(DataManager):
         except sqlite3.Error as e:
             raise DatabaseWritingError(query, e.args[0])
 
+    def insert_category(self, category: Category):
+        statement = "INSERT INTO categories VALUES ('" + str(category.category_id) + "','" + str(
+            category.name) + "'," + str(category.parent_id) + ");"
+        self._push(statement)
+
     def insert_series(self, series: Series):
-        statement = "INSERT INTO series VALUES ('" + str(series.series_id) + "'," + "'" + str(
+        statement = "INSERT INTO series VALUES ('" + str(series.series_id) + "','" + str(
             series.title) + "'" + ",'" + str(series.last_updated) + "'," + str(series.category_id) + ");"
         self._push(statement)
 
@@ -265,12 +290,12 @@ class Database(DataManager):
         statement = "DELETE  FROM series WHERE series_id='" + series.series_id + "';"
         self._push(statement)
 
-    def is_new_series(self,series: Series):
+    def is_new_series(self, series: Series):
         try:
             prev_series = self._get_single_series(series.series_id)
             last_date = dt.strptime(prev_series.last_updated.split(" ")[0], "%Y-%m-%d")
             actual_date = dt.strptime(series.last_updated.split(" ")[0], "%Y-%m-%d")
-            return actual_date > last_date
+            return actual_date > last_date or len(self.get_observables(series.series_id)) == 0
         except SeriesNotFound as e:
             return True
 
@@ -281,7 +306,7 @@ class Database(DataManager):
         for obs in observables:
             self.insert_observables(obs)
 
-    def _get_single_series(self, series_id: int) -> Series:
+    def _get_single_series(self, series_id: str) -> Series:
         statement = "SELECT * FROM series WHERE series_id ='" + str(series_id) + "';"
         rows = self._get(statement)
         series = self._parse(ModelType.Series, rows)
